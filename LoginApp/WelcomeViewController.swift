ae5d0c3a6e66dfad99af00f277c78137d8af17a5
YuriyGubin <iosdevelop.2022@gmail.com>
2022-10-04 20:19:39 +0500

    Final version

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 final class WelcomeViewController: UIViewController {
 
     @IBOutlet var welcomeLabel: UILabel!
-    @IBOutlet var backgroundView: UIView!
     
-    var greeting: String!
-    
-    var gradient: CAGradientLayer = {
+    var greeting = ""
+
+    private var gradient: CAGradientLayer = {
         let gradient = CAGradientLayer()
         gradient.colors = [
             #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1).cgColor,
@@ -25,9 +24,10 @@ final class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        welcomeLabel.text = "Welcome, \(greeting ?? "k")"
         gradient.frame = view.bounds
-        view.layer.addSublayer(gradient)
+        view.layer.insertSublayer(gradient, at: 0)
+        welcomeLabel.text = "Welcome, \(greeting)!"
     }
 }
+
+
