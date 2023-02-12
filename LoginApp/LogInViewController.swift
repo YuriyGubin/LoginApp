3d4e7bb660d19457727daf1d5b52fb4645e787b7
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-12 11:44:35 +0500

    Add outlets and actions for login screen

---
+++ LoginApp/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  LoginApp
+//
+//  Created by Yuriy on 12.02.2023.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func logInButtonPressed() {
+    }
+    
+    @IBAction func forgotUserNameButtonPressed() {
+    }
+    
+    @IBAction func forgotPasswordButtonPressed() {
+    }
+}
+
