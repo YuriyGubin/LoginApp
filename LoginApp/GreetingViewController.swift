02f52cdbe35c562bc82a2d44f1dfbbdcfe3fcb23
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-13 12:49:41 +0500

    Analysis of homework

--- LoginApp/GreetingViewController.swift
+++ LoginApp/GreetingViewController.swift
@@ -9,26 +9,29 @@ import UIKit
 
 final class GreetingViewController: UIViewController {
     
-    
     @IBOutlet var greetingLabel: UILabel!
     
-    private var gradient: CAGradientLayer = {
-        let gradient = CAGradientLayer()
-        gradient.colors = [
-            #colorLiteral(red: 1, green: 0.5301271778, blue: 0.7130295974, alpha: 1).cgColor,
-            #colorLiteral(red: 0.6380690239, green: 0.548365408, blue: 1, alpha: 1).cgColor
-        ]
-        return gradient
-    }()
+    private let firstColor = #colorLiteral(red: 1, green: 0.5301271778, blue: 0.7130295974, alpha: 1)
+    private let secondColor = #colorLiteral(red: 0.6380690239, green: 0.548365408, blue: 1, alpha: 1)
     
     var userName = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        gradient.frame = view.bounds
-        view.layer.insertSublayer(gradient, at: 0)
+        view.addVerticalGradient(topColor: firstColor, bottomColor: secondColor)
         greetingLabel.text = "Welcome, \(userName)!"
     }
-    
-   
+}
+
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradient(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
 }
--- LoginApp/LogInViewController.swift
+++ LoginApp/LogInViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
+    // MARK: - IBOutlets
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
     private let userName = "123"
     private let password = "123"
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
@@ -24,25 +25,27 @@ class LogInViewController: UIViewController {
         guard let greetingVC = segue.destination as? GreetingViewController else { return }
         greetingVC.userName = userNameTF.text ?? ""
     }
-
+    
+    // MARK: - IBActions
     @IBAction func logInButtonPressed() {
-        if userNameTF.text != userName || passwordTF.text != password {
+        guard userNameTF.text == userName, passwordTF.text == password else {
             showAlert(
                 withTitle: "Wrong User Name or Password 😎",
-                andMessage: "Enter correct User Name and Password, please!"
+                andMessage: "Enter correct User Name and Password, please!",
+                textField: passwordTF
             )
+            return
         }
+        performSegue(withIdentifier: "showWelcomeVC", sender: nil)
     }
     
-    @IBAction func forgotUserNameButtonPressed() {
-        showAlert(
+    @IBAction func forgotDataButtonsPressed(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(
             withTitle: "Your User Name is: \(userName)",
             andMessage: "Remember this already, please!"
         )
-    }
-    
-    @IBAction func forgotPasswordButtonPressed() {
-        showAlert(
+        : showAlert(
             withTitle: "Your Password is: \(password)",
             andMessage: "Remember this already, please!"
         )
@@ -53,19 +56,18 @@ class LogInViewController: UIViewController {
         userNameTF.text = ""
         passwordTF.text = ""
     }
-    
 }
 
+// MARK: - Set Alert
 extension LogInViewController {
-    private func showAlert(withTitle title: String, andMessage message: String) {
+    private func showAlert(withTitle title: String, andMessage message: String, textField: UITextField? = nil) {
         let alert = UIAlertController(
             title: title,
             message: message,
             preferredStyle: .alert
         )
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            self.userNameTF.text = ""
-            self.passwordTF.text = ""
+            textField?.text = ""
         }
         alert.addAction(okAction)
         present(alert, animated: true)
